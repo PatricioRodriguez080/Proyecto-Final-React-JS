5ee2a55ea7f8d2b9f9508c8de1338078c0cc47f9
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-06-13 19:12:41 -0300

    Creacion de componente navbar y cartWidget

---
+++ mi-app-react/src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CartWidget = () => {
+  return (
+    <div>
+        <i className="fa-solid fa-cart-shopping"></i>
+        <strong className='contador-elementos-carrito'>0</strong>
+    </div>
+  )
+}
