741f79d3a8dee94b54d5d7705176677c4e9b7d4a
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-06-16 17:13:10 -0300

    Cambios en la navbar, solucion de problema componente cartwidget

--- mi-app-react/src/components/Navbar.jsx
+++ mi-app-react/src/components/Navbar.jsx
@@ -4,27 +4,28 @@ import { CartWidget } from './CartWidget'
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Titulo</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <div className="container-fluid position-relative">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a className="navbar-brand" href="#">Brand</a>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
+              <a className="nav-link" href="#">Link</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
+              <a className="nav-link">Disabled</a>
             </li>
           </ul>
         </div>
+        <div className="position-absolute top-0 end-0">
+          <CartWidget />
+        </div>
       </div>
-      
-      <CartWidget/>
     </nav>
   )
 }
