ee8a2fdd4285db6087f0c47a3a7641e0d428530b
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-07-11 11:15:05 -0300

    Agregado de ruteo para los productos a ItemDetail

--- mi-app-react/src/components/Inicio/ItemListContainer/ItemListContainer.jsx
+++ mi-app-react/src/components/Inicio/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 export const ItemListContainer = ({ arrayProductos }) => {
   const { urlParam } = useParams()
@@ -19,13 +19,15 @@ export const ItemListContainer = ({ arrayProductos }) => {
       <div className="row row-item-list-container">
         {productosFiltrados.map(({ id, nombre, grupo, precio, imagen }) => (
           <div key={id} className="col col-item-list-container">
-            <div className="card" style={{ width: '18rem' }}>
-              <img src={imagen} className="card-img-top" alt={grupo} />
-              <div className="card-body card-body-nuevos-productos">
-                <p className="card-text">{nombre}</p>
-                <h4 className="mt-1">${precio}</h4>
+            <Link to={`/item/${id}`}>
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={imagen} className="card-img-top" alt={grupo} />
+                <div className="card-body card-body-nuevos-productos">
+                  <p className="card-text">{nombre}</p>
+                  <h4 className="mt-1">${precio}</h4>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
       </div>
