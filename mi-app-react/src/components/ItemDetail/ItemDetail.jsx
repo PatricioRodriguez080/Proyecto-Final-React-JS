2448e98f6b0d48eaf75d101919e59e61748be418
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-07-14 15:53:58 -0300

    Agregado de alerta en ItemDetail

--- mi-app-react/src/components/ItemDetail/ItemDetail.jsx
+++ mi-app-react/src/components/ItemDetail/ItemDetail.jsx
@@ -18,11 +18,11 @@ export const ItemDetail = ({ arrayProductos }) => {
   return (
     <div className="container-item-detail">
       <div className="card card-item-detail mb-3">
-        <div className="row g-0">
-          <div className="col-md-4">
+        <div className="row row-item-detail g-0">
+          <div className="col-md-5">
             <img src={productoAMostrar.imagen} className="img-fluid rounded-start" alt={productoAMostrar.nombre} />
           </div>
-          <div className="col-md-8">
+          <div className="col-md-7">
             <div className="card-body card-body-item-detail">
               <h5 className="card-title card-title-item-detail">{productoAMostrar.nombre}</h5>
               <p className="card-text card-text-item-detail">${productoAMostrar.precio}</p>
@@ -30,6 +30,12 @@ export const ItemDetail = ({ arrayProductos }) => {
                 <i className="fa-solid fa-minus "></i>
                 <p className="mb-0 mx-2">1</p>
                 <i className="fa-solid fa-plus"></i>
+              </div>
+              <div className="alert alert-dark d-flex align-items-center" role="alert">
+                <i className="fa-solid fa-info-circle me-2"></i>
+                <div>
+                  Scheduled Delivery Date is subject to product and logistics issues and may face delays. If the schedule is changed, customers will be notified individually.
+                </div>
               </div>
               <button type="button" className="btn btn-item-detail btn-lg">Large button</button>
             </div>
