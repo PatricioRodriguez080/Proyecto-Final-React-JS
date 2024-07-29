0f25bbceb680cccd323774a92599b8d2b243103f
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-07-29 20:03:31 -0300

    Eliminacion de prop arrayProductos de cartContext

--- mi-app-react/src/App.jsx
+++ mi-app-react/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-      <CartContextProvider arrayProductos={arrayProductos}>
+      <CartContextProvider>
         <BrowserRouter>
           <Navbar />
           <Routes>
